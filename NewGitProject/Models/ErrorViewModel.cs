e17a9c739711fb7050956e6ffacb80845d42cd4a
struikrovi1 <sabina20028@gmail.com>
2022-09-07 12:17:37 +0400

    add blog page

---
+++ NewGitProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NewGitProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
